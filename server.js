ea3fbaa2346699bc2a586508717f29c96e096284
Nilou Mostofi <nilou@km-direct.com>
2020-11-01 07:50:35 -0800

    deploying

--- server.js
+++ server.js
@@ -15,11 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 // routes
 app.use(require("./routes/api.js"));
 
