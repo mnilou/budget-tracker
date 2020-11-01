f773e810db81a7336f31971d34d7880f901af91b
Nilou Mostofi <nilou@km-direct.com>
2020-11-01 07:39:58 -0800

    added db.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,8 +3,10 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/index.js",
     "/db.js",
-    "/style.css"];
-
+    "/style.css",
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-512x512.png",
+];
     const CACHE_NAME = 'static-cache-v2';
     const DATA_CACHE_NAME = 'data-cache-v1';
 
